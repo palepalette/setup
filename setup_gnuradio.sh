4d18813e5f0c8a3a104c1d0b550cafe00bd4b4ca
Shunsuke Takagi <takagi@debian>
2015-08-04 17:38:06 +0900

    update

--- setup_gnuradio.sh
+++ setup_gnuradio.sh
@@ -3,5 +3,5 @@
 wget http://www.sbrac.org/files/build-gnuradio
 chmod a+x build-gnuradio
 yes | ./build-gnuradio
-sudo /usr/local/lib/uhd/utils/uhd_images_downloader.py
+#sudo /usr/local/lib/uhd/utils/uhd_images_downloader.py
 #/usr/local/lib/uhd/utils/usrp_n2xx_simple_net_burner
--- setup_vim.sh
+++ setup_vim.sh
@@ -3,5 +3,5 @@
 sudo apt-get install vim vim-gnome
 mkdir -p ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-cp setup/.vim ~/.
+cp .vimrc ~/.
 
